d820ef103caa6527edbf748af164aa7d7a29c2cd
Sunariyo87 <genarongalam87@gmail.com>
2021-04-26 21:56:15 +0700

    menambahkan Bootstrap dan menampilkan data mahasiswa

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -2,9 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Mahasiswa extends CI_Controller {
-
 	public function index()
 	{
-		$this->load->view('mhs/index');
+		$this->load->model('Mahasiswa_model');
+
+		$data['mahasiswas'] = $this->Mahasiswa_model->get_last_five_entries();
+
+		$this->load->view('templates/header');
+		$this->load->view('mhs/index',$data);
+		$this->load->view('templates/footer');
 	}
 }
--- application/views/mhs/index.php
+++ application/views/mhs/index.php
@@ -1,47 +1,47 @@
-    <div class="container">
+<div class="container">
         <div class="row">
         <div class="col-md-10">
             <h3>Biodata Mahasiswa</h3>
         </div>
         <div class="col-sm-2">
             <a class="btn btn-sm btn-success" href="{{ route('mhs.create') }}">Create New</a>
-            </div>    
-        </div>  
-        @if ($message = Session::get('success')) 
+            </div>
+        </div>
+        <!-- @if ($message = Session::get('success'))
             </div class="alert alert-success">
                 <p>{{"$message"}}</p>
              </div>
-    @endif
-    <table class="table table-hover table-sm">
-    <tr>
-    <th width="50px"><b>No.</b></th>
-    <th width="300px"><b>Nama Mahasiswa.</b></th>    
-    <th>Jenis Kelamin</th>
-    <th>Usia</th>
-    <th>Alamat</th>
-    <th width="180px">Action</th> 
-    <tr>
-    @foreach ($mahasiswas as $mahasiswa)
-    <tr>
-    <td><b>{{++$i}}.</b></td>
-    <td>{{$mahasiswa->nama}}</td>
-    <td>{{$mahasiswa->Gender}}</td>
-    <td>{{$mahasiswa->usia}}</td>
-    <td>{{$mahasiswa->alamat}}</td>
-    <td>
-    <form action="#" method="post">
-    <a class="btn btn-sm btn-primary" href="{{ route('mhs.show',$mahasiswa->id) }}">show</a>
-    <a class="btn btn-sm btn-warning" href="#">Edit</a>
-    
-    @csrf
-    @method('DELETE')
-    <button type="submit" class="btn btn-sm btn-danger">Delete</button>
-    </form>
-    </td>
-    </tr>
-    @endforeach
-    </table>
-    @endsection
-    {!! $mahasiswas->links() !!}
+        @endif -->
+ 
+        <table class="table table-hover table-sm">
+        <tr>
+        <th width="50px"><b>No.</b></th>
+        <th width="300px"><b>Nama Mahasiswa.</b></th>
+        <th>Jenis Kelamin</th>
+        <th>Usia</th>
+        <th>Alamat</th>
+        <th width="180px">Action</th>
+        <tr>
+        <?php foreach ($mahasiswas as $mahasiswa): ?>
+        <tr>
+        <td><b>{{++$i}}.</b></td>
+        <td><?php echo $mahasiswa->nama; ?></td>
+        <td><?php echo $mahasiswa->gender; ?></td>
+        <td><?php echo $mahasiswa->usia; ?></td>
+        <td><?php echo $mahasiswa->alamat; ?></td>
+        <td>
+        <form action="#" method="post">
+        <a class="btn btn-sm btn-primary" href="{{ route('mhs.show',$mahasiswa->id) }}">show</a>
+        <a class="btn btn-sm btn-warning" href="#">Edit</a>
+ 
+        @csrf
+        @method('DELETE')
+        <button type="submit" class="btn btn-sm btn-danger">Delete</button>
+        </form>
+        </td>
+        </tr>
+        <?php endforeach; ?>
+        </table>
+ 
+        {!! $mahasiswas->links() !!}
     </div>
-    
